48dc793c3118279f6f944c18d8737ccf85a21d30
cch98 <changho9808@gmail.com>
2019-04-11 15:36:25 +0900

    error fix

--- tutorial.py
+++ tutorial.py
@@ -69,7 +69,7 @@ class Style_loss(nn.Module):
         return input
 
 
-cnn = models.vgg19(pretrained=True).featurs.to(device).eval()
+cnn = models.vgg19(pretrained=True).features.to(device).eval()
 cnn_normalization_mean = torch.tensor([0.485, 0.456, 0.406]).to(device)
 cnn_normalization_std = torch.tensor([0.229, 0.224, 0.225]).to(device)
 
