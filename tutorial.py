aea0e09c99cf98e3f18bdac9c21c0357da43817f
cch98 <changho9808@gmail.com>
2019-04-11 15:54:30 +0900

    error fix

--- tutorial.py
+++ tutorial.py
@@ -115,14 +115,14 @@ def get_style_model_and_losses(cnn, normalization_mean, normalization_std, style
         model.add_module(name, layer)
 
         if name in content_layers:
-            target = model(content_image).detach()
+            target = model(content_img).detach()
             content_loss = Content_loss(target)
             model.add_module("content_loss_{}".format(i), content_loss)
             content_losses.append(content_loss)
             idx = i
 
         if name in style_layers:
-            target_feature = model(style_image).detach()
+            target_feature = model(style_img).detach()
             style_loss = Style_loss(target_feature)
             model.add_module("styel_loss_{}".format(i), style_loss)
             style_losses.append(style_loss)
