12716b319a3022c4094ee02e7c214b7e28f97ed3
cch98 <changho9808@gmail.com>
2019-04-16 22:58:11 +0900

    error fix:   File "tutorial.py", line 189, in run     image = unloader(input_image)   File "/home/ec2-user/test/lib/python3.6/site-packages/torchvision/transforms/transforms.py", line 126, in __call__     return F.to_pil_image(pic, self.mode)   File "/home/ec2-user/test/lib/python3.6/site-packages/torchvision/transforms/functional.py", line 116, in to_pil_image     raise ValueError('pic should be 2/3 dimensional. Got {} dimensions.'.format(pic.ndimension())) ValueError: pic should be 2/3 dimensional. Got 4 dimensions.

--- tutorial.py
+++ tutorial.py
@@ -184,7 +184,8 @@ def run(cnn, normalization_mean, normalization_std, content_img, style_img, inpu
             return style_score+content_score
 
         optimizer.step(closure)
-        image = unloader(input_image)
+        image = input_image.data.clamp_(0,1)
+        image = unloader(image)
         writer.add_image(image, iteration[0])
 
 
